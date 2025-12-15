6e9d81ca2e1008d90c5e85caf7c621f3d410ef3b
sumitx1kourav <sumitx1kourav@gmail.com>
2025-12-16 00:34:59 +0530

    ObstacleManager Scripts

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+// Spawns obstacles based on ObstacleData
+public class ObstacleManager : MonoBehaviour
+{
+    public ObstacleData obstacleData;
+    public GameObject obstaclePrefab;
+    public float spacing = 1.05f;
+
+    void Start()
+    {
+        SpawnObstacles();
+    }
+
+    void SpawnObstacles()
+    {
+        for (int x = 0; x < obstacleData.width; x++)
+        {
+            for (int y = 0; y < obstacleData.height; y++)
+            {
+                if (obstacleData.IsBlocked(x, y))
+                {
+                    Vector3 pos = new Vector3(
+                        x * spacing,
+                        0.25f,
+                        y * spacing
+                    );
+
+                    Instantiate(obstaclePrefab, pos, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
